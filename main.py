8da26ed9c34dbecf54a015f921311872184ad135
Vxidh <75539326+Vxidh@users.noreply.github.com>
2022-11-02 06:39:15 +0530

    Minor Changes

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 import PySimpleGUI as psg
 #set the theme for the screen/window
 psg.theme('SandyBeach')
-#define layout
+#Defining the layout of the window
 layout=[[psg.Text('Choose Year',size=(20, 1), font='Lucida',justification='left')],
         [psg.Combo(['2015','2016','2017', '2018','2019','2020','2021','2022'],key='year')],
         [psg.Text('Choose Team',size=(30, 1), font='Lucida',justification='left')],
-        [psg.Combo(['Arsenal','Manchester City','Tottenham Hotspurs', 'Chelsea','Manchester United',
-        'Liverpool','Leicester City','West Ham'],key='team')],
-         [psg.Text('Choose additional Facilities',size=(30, 1), font='Lucida',justification='left')],
-         #[psg.Listbox(values=['Welcome Drink', 'Extra Cushions', 'Organic Diet','Blanket', 'Neck Rest'], select_mode='extended', key='fac', size=(30, 6))],
-        [psg.Button('SAVE', font=('Times New Roman',12)),psg.Button('CANCEL', font=('Times New Roman',12))]]
-#Define Window
-win =psg.Window('Customise your Journey',layout)
-#Read  values entered by user
-e,v=win.read()
-#close first window
-win.close()
-# #access the selected value in the list box and add them to a string
-# strx=""
-# for val in v['fac']:
-#     strx=strx+ " "+ val+","
-        
-#display string in a popup         
-psg.popup('Team chosen is: '+v['team']+' from the year: '+v['year'])
+        #psg.combo gives you a drop-down list
+        [psg.Combo(['Arsenal','Manchester City','Tottenham Hotspurs', 'Newcastle','Chelsea','Manchester United','Fulham',
+        'Liverpool','Brighton','West Ham','Brentford','Everton','Crystal Palace','Bournemouth',
+        'Aston Villa','Southampton','Leicester City','Leeds United','Wolves','Nottingham Forest'],key='team')], #Teams playing in the Premier League
+        [psg.Text('Choose additional Facilities',size=(30, 1), font='Lucida',justification='left')],
+        [psg.Button('SAVE', font=('Times New Roman',12)),psg.Button('CANCEL', font=('Times New Roman',12))]] #Save button and cancel button
+
+while True:
+        #Define Window
+        win =psg.Window('Expected Goals',layout)
+        #Read  values entered by user
+        e,v=win.read()
+        if e == sg.WINDOW_CLOSED or e == 'Quit':
+                break
+        #close first window
+        win.close()
+        # #access the selected value in the list box and add them to a string
+        # strx=""
+        # for val in v['fac']:
+        #     strx=strx+ " "+ val+","
+                
+        #display string in a popup         
+        psg.popup('Team chosen is: '+v['team']+' from the year: '+v['year'])
 
