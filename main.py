94d05a8e6b73740aedaf2ebb14fd72dfa52f80b8
Vxidh <75539326+Vxidh@users.noreply.github.com>
2022-11-03 08:51:15 +0530

    Major changes made

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import asyncio
+import json
+from understat import Understat
+import aiohttp
 import PySimpleGUI as psg
 #set the theme for the screen/window
 psg.theme('SandyBeach')
@@ -6,26 +10,30 @@ layout=[[psg.Text('Choose Year',size=(20, 1), font='Lucida',justification='left'
         [psg.Combo(['2015','2016','2017', '2018','2019','2020','2021','2022'],key='year')],
         [psg.Text('Choose Team',size=(30, 1), font='Lucida',justification='left')],
         #psg.combo gives you a drop-down list
-        [psg.Combo(['Arsenal','Manchester City','Tottenham Hotspurs', 'Newcastle','Chelsea','Manchester United','Fulham',
+        [psg.Combo(['Arsenal','Manchester City','Tottenham', 'Newcastle United','Chelsea','Manchester United','Fulham',
         'Liverpool','Brighton','West Ham','Brentford','Everton','Crystal Palace','Bournemouth',
-        'Aston Villa','Southampton','Leicester City','Leeds United','Wolves','Nottingham Forest'],key='team')], #Teams playing in the Premier League
+        'Aston Villa','Southampton','Leicester','Leeds','Wolverhampton Wanderers','Nottingham Forest'],key='team')], #Teams playing in the Premier League
         [psg.Text('Choose additional Facilities',size=(30, 1), font='Lucida',justification='left')],
         [psg.Button('SAVE', font=('Times New Roman',12)),psg.Button('CANCEL', font=('Times New Roman',12))]] #Save button and cancel button
-
-while True:
-        #Define Window
-        win =psg.Window('Expected Goals',layout)
-        #Read  values entered by user
-        e,v=win.read()
-        if e == sg.WINDOW_CLOSED or e == 'Quit':
-                break
-        #close first window
+#Function to get data from understat
+def team_data():
+    async def main():
+            async with aiohttp.ClientSession() as session:
+                understat = Understat(session)
+                data = await understat.get_league_players("epl", v['year'], team_title= v['team'])
+                print(json.dumps(data))
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+#Define Window
+win =psg.Window('Expected Goals',layout)
+#Read  values entered by user
+e,v=win.read()
+if e == psg.WINDOW_CLOSED or e == 'Quit':
         win.close()
-        # #access the selected value in the list box and add them to a string
-        # strx=""
-        # for val in v['fac']:
-        #     strx=strx+ " "+ val+","
-                
-        #display string in a popup         
-        psg.popup('Team chosen is: '+v['team']+' from the year: '+v['year'])
+else:
+        win.close()
+        team_data()
+
+
+
 
--- understat.py
+++ understat.py
@@ -11,19 +11,14 @@ import json
 import aiohttp
 
 from understat import Understat
-from bs4 import BeautifulSoup
-with open("https://fbref.com/en/comps/9/Premier-League-Stats") as fp:
-    soup = BeautifulSoup(fp, 'html.parser')
-print(BeautifulSoup("<html><head></head><body>Sacr&eacute; bleu!</body></html>", "html.parser"))
-
-# async def main():
-#     async with aiohttp.ClientSession() as session:
-#         understat = Understat(session)
-#         data = await understat.get_league_players("epl", 2018, {"team_title": "Manchester City"})
-#         print(json.dumps(data))
+async def main():
+    async with aiohttp.ClientSession() as session:
+        understat = Understat(session)
+        data = await understat.get_league_players("epl", 2018, {"team_title": "Manchester City"})
+        print(json.dumps(data))
 
 
-# if __name__ == "__main__":
-#     loop = asyncio.new_event_loop()
-#     asyncio.set_event_loop(loop)
-#     loop.run_until_complete(main())
+if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
