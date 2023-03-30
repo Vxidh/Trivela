d9b9f5ec3f06d21c1e87a7116d9f3e2ab477257a
Vaidheesh Bharadwaj <75539326+Vxidh@users.noreply.github.com>
2023-03-30 18:43:08 +0530

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-text='Python is the best programming language in the world in 2020 Edureka is the biggest Ed-tech platform to learn python Python programming is as easy as writing a program in simple English language '
-with open('example.txt','w') as f1:
-    f1.write(text)
-with open('example.txt','r') as f2:
-    t1=f2.readline(5)
-    print(t1)
